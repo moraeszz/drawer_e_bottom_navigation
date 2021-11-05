d400e3c348171a74f25138849330e0e2941ab7d6
rafaela <rafaelaandrademoraesz@gmail.com>
2021-11-05 11:07:10 -0300

    drawer e bottom navigation

---
+++ app/src/main/java/com/example/bottomnavigationfragment/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.example.bottomnavigationfragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Button
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    // Botôes
+    lateinit var btnHome: Button
+    lateinit var btnCliente: Button
+    lateinit var btnBusca: Button
+
+    //Fragmentos
+    lateinit var homeFragment: HomeFragment
+    lateinit var clienteFragment: ClientesFragment
+    lateinit var buscaFragment: BuscaFragment
+
+    //Toolbar
+    lateinit var toolbar: Toolbar
+
+    lateinit var bottomNavigation: BottomNavigationView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnBusca = findViewById(R.id.btn_Busca)
+        btnCliente = findViewById(R.id.btn_clientes)
+        btnHome = findViewById(R.id.btn_home)
+
+        buscaFragment = BuscaFragment()
+        clienteFragment = ClientesFragment()
+        homeFragment = HomeFragment()
+
+        bottomNavigation = findViewById(R.id.bottom_navigation)
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        toolbar.setTitle("Teste Drawer")
+
+        setFragment(homeFragment)
+
+        btnHome.setOnClickListener {
+            setFragment(homeFragment)
+        }
+
+        btnCliente.setOnClickListener {
+            setFragment(clienteFragment)
+        }
+
+        btnBusca.setOnClickListener {
+            setFragment(buscaFragment)
+        }
+
+        bottomNavigation.setOnItemSelectedListener { item ->
+            onNavigationItemSelected(item)
+            true
+        }
+    }
+
+    private fun setFragment(fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame, fragment)
+        fragmentTransaction.commit()
+    }
+
+    fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.menu_home -> {
+                setFragment(homeFragment)
+            }
+            R.id.menu_cliente -> {
+                setFragment(clienteFragment)
+            } else -> {
+                setFragment(buscaFragment)
+            }
+        }
+
+        return true
+    }
+
+}
